9b813bffe262ef52bedbefbf4f6044fe22f71b9a
hcc <hcc@163.com>
2022-11-23 21:41:04 +0800

    1

--- pytorch/tensorflow/test.py
+++ pytorch/tensorflow/test.py
@@ -1,7 +1,9 @@
 # author: hcx
 import tensorflow as tf
-import numpy as np
+print("------")
+with tf.GradientTape() as tape:
+    w = tf.Variable(tf.constant(3,0))
+    loss = tf.pow(w, 2)
+grad = tape.gradient(loss, w)
+print(grad)
 
-# 生成均匀分布随机数
-a = tf.random.uniform([2,3], maxval=1, minval=0)
-print(a)
